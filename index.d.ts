253880ab1416fbf96ed7cbc58b9543c91b90569a
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2017-08-25 00:06:36 +0200

    Adjusted declaration file.

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,2 @@
-export declare function exec(command: string): Promise<Standard>;
-export declare function execFile(file: string, options?: string[]): Promise<Standard>;
-export declare class Standard {
-    out: string;
-    err: string;
-    constructor(out: string, err: string);
-}
+export declare function exec(command: string): Promise<{ stdout: string; stderr: string }>;
+export declare function execFile(file: string, options?: string[]): Promise<{ stdout: string; stderr: string }>;
