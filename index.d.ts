f945ccf506f9816088205d14501604c3571f5313
Fabian Pirklbauer <fabian.pirklbauer@gmail.com>
2017-12-04 09:35:10 +0100

    Removed build.

--- index.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function exec(command: string): Promise<{ stdout: string; stderr: string }>;
-export declare function execFile(file: string, options?: string[]): Promise<{ stdout: string; stderr: string }>;
